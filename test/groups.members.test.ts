ee5681904b7aa64748fb20e81b660ddf6a186dd4
zpr1g <yuns.xie@qq.com>
2020-08-23 18:25:23 +0800

    chore: update test instance

--- test/groups.members.test.ts
+++ test/groups.members.test.ts
@@ -1,10 +1,9 @@
 import kit48 from '../src';
 
-describe('groups', () => {
-  it('get all members of group SNH48', () => {
-    return kit48.groups.members(kit48.groups.members.Groups.SNH48).then((response) => {
-      console.log('SNH48 members count:', response.data.rows.length);
-      expect(response.data.rows.length > 0);
-    });
+describe('groups members', () => {
+  it('get all members of group SNH48', async () => {
+    const response = await kit48.groups.members(kit48.groups.members.Groups.SNH48);
+    console.log('count:', response.data.rows.length);
+    expect(response.data.rows.length > 0);
   });
 });
